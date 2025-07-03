68c802c8adfbc2e52109c63932ef7016a6280683
Rakindu Niwunhella <rakindu15@gmail.com>
2025-07-03 18:45:39 +0530

    d

---
+++ Database/Doctors.sql
@@ -0,0 +1,12 @@
+create table doctors(
+id INT PRIMARY KEY,
+NAME varchar(100),
+email varchar(100),
+phone varchar(10),
+specialization varchar(30),
+availability date,
+hospital varchar(30),
+profile_pic varchar(100),
+rating decimal (2,1)
+
+);
