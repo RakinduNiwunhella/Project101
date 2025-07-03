05b5811f1512c57270d3d463ac091514ee17f5a3
Rakindu Niwunhella <rakindu15@gmail.com>
2025-07-03 20:00:45 +0530

    ds

--- Database/Doctors.sql
+++ Database/Doctors.sql
@@ -1,12 +1,19 @@
-create table doctors(
-id INT PRIMARY KEY,
-NAME varchar(100),
-email varchar(100),
-phone varchar(10),
-specialization varchar(30),
-availability date,
-hospital varchar(30),
-profile_pic varchar(100),
-rating decimal (2,1)
+insert into doctors()
+values(
+4,
+"Dr. Michael Brown",
+"MichelB@gmail.com",
+"0770112236",
+"Ayurvedic & Holistic",
+"2025-07-2",
+"Hospital001",
+"image-URL",
+4.5
+
+
 
 );
+
+
+
+select * from doctors;
